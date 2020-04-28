fd3288c7dd026e469268e0e2a45b49058575848c
emmehandes <emmehandes@gmail.com>
2020-04-28 23:09:11 +0100

    building resolving server

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,60 @@
 mod protocol;
-use std::io::Read;
-use std::fs::File;
+use std::io::Error;
 use std::net::UdpSocket;
 use protocol::Limits;
 use protocol::Packet;
 use protocol::QueryType;
 use protocol::Question;
+use protocol::ResultCode;
+
+fn lookup(qname: &str, qtype: QueryType, server: (&str, u16)) -> Result<Packet, Error> {
+    let mut request: [u8; Limits::Size as usize] = [0; Limits::Size as usize];
+    let mut response: [u8; Limits::Size as usize] = [0; Limits::Size as usize];
+    let mut packet = Packet::new();
+    let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
+
+    packet.header.id = 6666;
+    packet.header.questions = 1;
+    packet.header.recursion_desired = true;
+    packet.questions.push(Question::build(&qname.to_string(), qtype));
+    packet.read(&mut request)?;
+
+    socket.send_to(&mut request, server).unwrap();
+    packet.write(&mut response)?;
+    Ok(packet)
+}
 
 fn main() {
-    // TEST 1
-    println!("\n------------------------ TEST PACKET PARSING");
-    let mut f = File::open("response_packet.txt").unwrap();
+    let server = ("8.8.8.8", 53);
+    let local = ("0.0.0.0", 2053);
     let mut array: [u8; Limits::Size as usize] = [0; Limits::Size as usize];
-    f.read(&mut array).unwrap();
+    let mut request = Packet::new();
 
-    let mut packet = Packet::new();
-    packet.write(&array).unwrap();
-    println!("{:#?}", packet.header);
+    let socket = UdpSocket::bind(local).unwrap();
 
-    for q in packet.questions {
-        println!("{:#?}", q);
-    }
-    for rec in packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in packet.resources {
-        println!("{:#?}", rec);
-    }
+    // Sequentials queries handling
+    loop {
+        let (_, src) = match socket.recv_from(&mut array) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("Failed to read from UDP socket: {:?}", e);
+                continue;
+            }
+        };
+        request.read(&mut array).expect("Failed to parse UDP query packet");
 
-    // TEST 2
-    // ----------------- SEND ------------------------------
-    println!("\n------------------------ TEST SEND/RCV");
-    let server = ("8.8.8.8", 53); let socket = UdpSocket::bind(("0.0.0.0", 4321)).unwrap(); // Prepare packet
-    let domain_name = "cloudflare.com";
-    let qtype = QueryType::A;
+        let mut response = Packet::new();
+        response.header.id = request.header.id;
+        response.header.recursion_desired = true;
+        response.header.recursion_available = true;
+        response.header.response = true;
 
-    let mut packet = Packet::new();
-    packet.header.id = 6666;
-    packet.header.recursion_desired = true;
-    packet.questions.push(Question::build(domain_name, qtype));
-    packet.header.questions = 1;
-
-    let mut send_array: [u8; Limits::Size as usize] = [0; Limits::Size as usize];
-    packet.read(&mut send_array).unwrap();
-
-    // send packet
-    socket.send_to(&send_array, server).unwrap();
-
-
-    // ----------------- RECEIVE ---------------------------
-    let mut recv_buffer: [u8; Limits::Size as usize] = [0; Limits::Size as usize];
-    socket.recv_from(&mut recv_buffer).unwrap();
-
-    let mut recv_packet = Packet::new();
-    for elem in recv_buffer.iter() {
-        print!("{:#?} ", elem);
-    }
-    println!("\n LENGTH: {length}", length=recv_buffer.len());
-    recv_packet.write(&recv_buffer).unwrap();
-    println!("{:#?}", recv_packet.header);
-
-    for q in recv_packet.questions {
-        println!("{:#?}", q);
-    }
-    for rec in recv_packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in recv_packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in recv_packet.resources {
-        println!("{:#?}", rec);
+        if request.questions.is_empty() {
+            response.header.rescode = ResultCode::FORMERR;
+        }
+        else {
+            let question = &request.questions[0];
+            println!("Received query: {:?}", question);
+        }
     }
 }
